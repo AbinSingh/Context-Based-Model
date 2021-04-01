77d35972c52b7f4ee7dd6ed92f1a11a63cab2de4
Abin Singh Rajan <abin.astro@gmail.com>
2021-04-02 00:04:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def get_delay():
     
     print(mydict)
     
-    return render_template('home.html')
+    return render_template('result.html',user_data=mydict)
       
 
 if __name__ == '__main__':
